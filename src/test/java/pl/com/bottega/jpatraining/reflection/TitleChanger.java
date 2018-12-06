3a9e6f069b23458c7bff3695392b71e10d7d2bf9
Maciej Zubala <maciej.zubala@allegro.pl>
2018-12-06 10:27:49 +0100

    reflection solution

--- src/test/java/pl/com/bottega/jpatraining/reflection/TitleChanger.java
+++ src/test/java/pl/com/bottega/jpatraining/reflection/TitleChanger.java
@@ -1,14 +1,33 @@
 package pl.com.bottega.jpatraining.reflection;
 
+import java.lang.reflect.Field;
+
 public class TitleChanger {
 
     public void changeTitle(Object object, String newTitle) {
-        // TODO
+        try {
+            Field title = object.getClass().getDeclaredField("title");
+            title.setAccessible(true);
+            title.set(object, newTitle);
+            title.setAccessible(false);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            //nothing to do here
+        }
     }
 
     public String getTitle(Object object) {
-        // TODO
-        return null;
+        try {
+            Field title = object.getClass().getDeclaredField("title");
+            title.setAccessible(true);
+            String val = (String) title.get(object);
+            title.setAccessible(false);
+            return val;
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            return null;
+        }
     }
-
 }
