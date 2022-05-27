0aaeb660f1b2f11320a5358151955ecdc62c5341
Maciej Zubala <maciejzubala@Maciejs-MacBook-Pro.local>
2022-05-27 20:48:25 +0200

    added spring app

---
+++ spring/src/main/java/pl/com/bottega/jpatraining/spirng/App.java
@@ -0,0 +1,11 @@
+package pl.com.bottega.jpatraining.spirng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
