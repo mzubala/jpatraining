e0601a80935dbf3ed670645ce6bf852f9b7f2df1
Maciej Zubala <maciej.zubala@gmail.com>
2022-05-30 08:57:15 +0200

    added spring app

---
+++ spring/src/main/java/pl/com/bottega/jpatraining/spirng/ProductController.java
@@ -0,0 +1,23 @@
+package pl.com.bottega.jpatraining.spirng;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManager;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+@Log
+public class ProductController {
+
+    private final EntityManager entityManager;
+
+    @GetMapping
+    public void getProduct() {
+        log.info(entityManager.getClass().toString());
+    }
+}
