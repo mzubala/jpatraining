ee87f47fba0fc665967c445a0c6ae002a0acb54a
Maciej Zubala <maciej.zubala@gmail.com>
2023-04-21 16:48:12 +0200

    solution

--- basics/src/test/java/pl/com/bottega/jpatraining/locking2/Room.java
+++ basics/src/test/java/pl/com/bottega/jpatraining/locking2/Room.java
@@ -1,7 +1,14 @@
 package pl.com.bottega.jpatraining.locking2;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import java.time.LocalDate;
 
 @Entity
 class Room {
@@ -12,5 +19,58 @@ class Room {
 
     public Room(Long id) {
         this.id = id;
+    }
+}
+
+@Entity
+@Table(
+    name = "reservations",
+    indexes = {
+        @Index(columnList = "fromInclusive"),
+        @Index(columnList = "untilExclusive"),
+        @Index(columnList = "customerId")
+    })
+@NamedQuery(
+    name = Reservation.COLLISIONS_EXIST_QUERY, // makeResrvation
+    query = "SELECT count(r) > 0 " +
+        "FROM Reservation r " +
+        "WHERE r.room.id = :roomId AND ((:start <= r.fromInclusive AND :end > r.fromInclusive) " +
+        "OR (:start >= r.fromInclusive AND :end <= r.untilExclusive) " +
+        "OR (:start < r.untilExclusive AND :end >= r.untilExclusive))"
+)
+@NamedQuery(
+    name = Reservation.IS_RESERVED_QUERY, // isReserved
+    query = "SELECT count(r) > 0 " +
+        "FROM Reservation r " +
+        "WHERE r.room.id = :roomId AND :at >= r.fromInclusive AND :at < r.untilExclusive AND r.customerId = :customerId"
+)
+@NamedQuery(
+    name = Reservation.COUNT_RESERVATIONS, // count
+    query = "SELECT count(r) FROM Reservation r"
+)
+class Reservation {
+
+    static final String COLLISIONS_EXIST_QUERY = "COLLISIONS_EXIST_QUERY";
+    static final String COUNT_RESERVATIONS = "COUNT_RESERVATIONS";
+    static final String IS_RESERVED_QUERY = "IS_RESERVED_QUERY";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    Room room;
+
+    Long customerId;
+
+    LocalDate fromInclusive;
+
+    LocalDate untilExclusive;
+
+    public Reservation(Room room, Long customerId, LocalDate fromInclusive, LocalDate untilExclusive) {
+        this.room = room;
+        this.customerId = customerId;
+        this.fromInclusive = fromInclusive;
+        this.untilExclusive = untilExclusive;
     }
 }
--- basics/src/test/java/pl/com/bottega/jpatraining/locking2/RoomReservationService.java
+++ basics/src/test/java/pl/com/bottega/jpatraining/locking2/RoomReservationService.java
@@ -2,6 +2,7 @@ package pl.com.bottega.jpatraining.locking2;
 
 import pl.com.bottega.jpatraining.EntityManagerTemplate;
 
+import javax.persistence.LockModeType;
 import java.time.LocalDate;
 
 class RoomReservationService {
@@ -13,15 +14,31 @@ class RoomReservationService {
     }
 
     public void makeReservation(MakeReservationCommand command) throws RoomNotAvailableException {
-
+        entityManagerTemplate.executeInTx((em) -> {
+            var room = em.find(Room.class, command.roomId);
+            Boolean colistionsExist = em.createNamedQuery(Reservation.COLLISIONS_EXIST_QUERY, Boolean.class)
+                .setParameter("start", command.fromInclusive)
+                .setParameter("end", command.untilExclusive)
+                .setParameter("roomId", command.roomId)
+                .getSingleResult();
+            if(colistionsExist) {
+                throw new RoomNotAvailableException();
+            }
+            em.persist(new Reservation(
+                room, command.customerId, command.fromInclusive, command.untilExclusive
+            ));
+        });
     }
 
     public boolean isReserved(ReservationQuery reservationQuery) {
-        return false;
+        return entityManagerTemplate.getEntityManager().createNamedQuery(Reservation.IS_RESERVED_QUERY, Boolean.class)
+            .setParameter("at", reservationQuery.at)
+            .setParameter("roomId", reservationQuery.roomId)
+            .setParameter("customerId", reservationQuery.customerId).getSingleResult();
     }
 
     public Integer reservationsCount() {
-        return 0;
+        return entityManagerTemplate.getEntityManager().createNamedQuery(Reservation.COUNT_RESERVATIONS, Long.class).getSingleResult().intValue();
     }
 }
 
